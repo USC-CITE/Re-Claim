d69d05bfe0d7330df907904685d429c044b4a172
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-01-21 06:02:07 +0800

    Fix router argument field error

--- public/index.php
+++ public/index.php
@@ -27,7 +27,7 @@ $router->get('/', function () {
 });
 $router->get('/login', [AuthController::class, 'showLogin']);
 $router->post('/login', [AuthController::class, 'login']);
-$router->get('/register', [AuthController::class], 'showRegister');
+$router->get('/register', [AuthController::class, 'showRegister']);
 $router->post('/register', [AuthController::class, 'register']);
 
 $router->dispatch();
