980383fea0f2c5f46a46b68a9d4e89f31c9816c2
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-01-21 12:34:00 +0800

    Add register page business logic handler

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -7,6 +7,10 @@
 
 namespace App\Controllers;
 
+use App\Core\Database;
+use App\Models\UserModel;
+
+
 class AuthController{
     public static function showLogin(){
         require __DIR__ . '/../Views/auth/login.php';
@@ -20,12 +24,47 @@ class AuthController{
         if($email === 'test@wvsu.edu.ph' && $password === '123'){
             echo 'Test successful!';
             return;
-        }
+        }   
         echo 'Invalid credentials!';
     }
 
     public static function showRegister(){
         require __DIR__ . '/../Views/auth/register.php';
+    }
+
+    public static function register(array $config){
+        // Establish database connection
+        $db = Database::connect($config['db']);
+
+        // Handle data submitted by the user
+        $firstName = trim($_POST["firstname"] ?? "");
+        $lastName = trim($_POST["lastname"] ?? "");
+        $email = trim($_POST["email"] ?? "");
+        $password = trim($_POST["password"] ?? "");
+        $confirmPass = trim($_POST["confirm-pass"] ?? "");
+
+        // Validate data
+        if(!$firstName || !$lastName || !$email || !$password || !$confirmPass){
+            echo "All fields are mandatory!";
+            return;
+        }
+
+        // Check password if matching
+        if($password !== $confirmPass){
+            echo "Password does not match!";
+            return;
+        }
+
+       // Utilize UserModel create method to insert into database
+        $model = new UserModel($config);
+        $success = $model->create([ 
+            'first_name' => $firstName,
+            'last_name' => $lastName, 
+            'email' => $email, 
+            'password' => $password,
+            'phone' => ' ' ]);
+
+        echo $success ? 'User registered successfully!' : 'Registration failed.';
     }
 }
 
