acaa271af3517d090892c96fb4d54cc1c40e1d80
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-01-21 12:39:32 +0800

    Add UserModel.php to handle database logic and sql operations related to USERS table

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -1,6 +1,36 @@
-<?php 
+<?php
 /**
  * Layer: Model
- * Purpose: Business logic and data handling
- * Rules: No HTTP or view rendering logic
+ * Purpose: Database logic and data handling
+ * Rules: No HTTP or view rendering logic; DB logic only
  */
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use App\Core\Database;
+use PDO;
+
+class UserModel {
+    private PDO $db;
+
+    public function __construct(array $config){
+        $this->db = Database::connect($config['db']);
+    }
+
+    public function create(array $data): bool {
+        $sql = "INSERT INTO users (first_name, last_name, wvsu_email, password, phone_number) 
+                VALUES (:first, :last, :email, :password, :phone)";
+
+        $stmt = $this->db->prepare($sql);
+
+        return $stmt->execute([
+            'first'    => $data['first_name'],
+            'last'     => $data['last_name'],
+            'email'    => $data['email'],
+            'password' => password_hash($data['password'], PASSWORD_DEFAULT),
+            'phone'    => $data['phone_number'] ?? '' 
+        ]);
+    }
+}
