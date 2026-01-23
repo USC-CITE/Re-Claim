f9ea2b57a61082c72552b5ad54a4f2a79533024f
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-01-23 22:48:07 +0800

    Add .env file to hide sensitive information

--- src/Config/config.php
+++ src/Config/config.php
@@ -5,13 +5,24 @@
  * Rules: Hide sensitive information
  */
 
+$envPath = dirname(__DIR__, 2) .'/.env';
+
+// Handle error if file does not exist
+if (!file_exists($envPath)) {
+    var_dump($envPath);
+    throw new Exception('.env file not found');
+}
+
+// Parse the fields inside the .env into an array
+$env = parse_ini_file($envPath);
+
 return[
     'db' => [
-        'host' => 'reclaim-database',
-        'port' => '3306',
-        'name' => 'reclaim_app',
-        'user' => 'root',
-        'pass' => '123',
-        'charset' => 'utf8mb4',
+        'host' => $env['DB_CONNECTION'],
+        'port' => $env['DB_PORT'],
+        'name' => $env['DB_NAME'],
+        'user' => $env['DB_USER'],
+        'pass' => $env['DB_PASS'],
+        'charset' => $env['DB_CHARSET'],
     ],
 ];
