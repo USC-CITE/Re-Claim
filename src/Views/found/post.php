22fa254f938431507f2a2607349d9a1fb03c5ab8
Fabshkiee <pwedefabros@gmail.com>
2026-01-31 10:05:08 +0800

    add location found options

--- src/Views/found/post.php
+++ src/Views/found/post.php
@@ -31,6 +31,42 @@
                 <img id="preview-image" alt="Image Preview">
             </div>
         </fieldset>
+        
+        <fieldset>
+            <legend>Location Found</legend>
+            <label>
+                Select Location:
+                <select name="location" id="location" required>
+                    <option value="">-- Select Location --</option>
+                    <option value="General Services Office|10.713457,122.559756">General Services Office</option>
+                    <option value="Office of Student Affairs|10.712972,122.563018">Office of Student Affairs</option>
+                    <option value="WVSU Cooperative|10.712769,122.561156">WVSU Cooperative</option>
+                    <option value="Lopez Jaena Building / ULRC|10.713988,122.561620">Lopez Jaena Building / ULRC</option>
+                    <option value="Quezon Hall|10.713140,122.562691">Quezon Hall</option>
+                    <option value="Rizal Hall|10.713691,122.561374">Rizal Hall</option>
+                    <option value="CBM Building / Claro M. Recto Hall|10.712056,122.563951">CBM Building / Claro M. Recto Hall</option>
+                    <option value="COM Building / Roxas Hall|10.712829,122.561771">COM Building / Roxas Hall</option>
+                    <option value="CON Building|10.713180,122.560886">CON Building</option>
+                    <option value="COC Building|10.714665,122.562294">COC Building</option>
+                    <option value="CICT Building|10.7132169,122.5615582">CICT Building</option>
+                    <option value="COD Building|10.712382,122.563600">COD Building</option>
+                    <option value="BINHI TBI|10.712405,122.560322">BINHI TBI</option>
+                    <option value="WVSU Grandstand|10.713844,122.562986">WVSU Grandstand</option>
+                    <option value="WVSU Cultural Center|10.714734,122.562701">WVSU Cultural Center</option>
+                    <option value="Center for Teaching Excellence|10.712382,122.563600">Center for Teaching Excellence</option>
+                    <option value="Administration Building|10.714665,122.562294">Administration Building</option>
+                    <option value="Audio Visual Room|10.714481,122.562312">Audio Visual Room</option>
+                    <option value="Mini Forest|10.713539,122.562146">Mini Forest</option>
+                    <option value="Diamond Park|10.713873,122.562240">Diamond Park</option>
+                    <option value="WVSU Multi-Purpose Cooperative|10.715193,122.562688">WVSU Multi-Purpose Cooperative</option>
+                    <option value="WVSU Cafeteria|10.712835,122.562814">WVSU Cafeteria</option>
+                    <option value="WVSU Hometel|10.712835,122.562758">WVSU Hometel</option>
+                    <option value="WVSU Research and Extension Building II|10.712846,122.560650">WVSU Research and Extension Building II</option>
+                    <option value="WVSU Research and Extension Building I|10.712661,122.560491">WVSU Research and Extension Building I</option>
+                    <option value="PESCAR Building / Ramon Magsaysay Hall|10.712845,122.563332">PESCAR Building / Ramon Magsaysay Hall</option>
+                    <option value="New Academic Building|10.713086,122.563506">New Academic Building</option>
+                </select>
+            </label>
 </main>
 
 
