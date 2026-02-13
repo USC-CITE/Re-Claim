adacb43d05a2ab6c82aeb283fc8068406f11ac9a
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-02-14 00:23:09 +0800

    Add method to fetch items from lost and found table by item type

--- src/Controllers/ProfileController.php
+++ src/Controllers/ProfileController.php
@@ -5,7 +5,21 @@ use App\Models\UserModel;
 
 class ProfileController{
     public static function showProfile(){
+        if (!isset($_SESSION['user_id'])) {
+            header("Location: /login");
+            exit;
+        }
+        $id = $_SESSION['user_id'];
+        
+        $config = require __DIR__ . "/../Config/config.php";
+
+        $user = new UserModel($config);
+        
+        $lostItems = $user->fetchItems($id, "lost");
+        $foundItems = $user->fetchItems($id, "found");
+
         require __DIR__ . "/../Views/mainpages/profile.php";
+
     }
     public static function showEditProfile(){
         require __DIR__ . '/../Views/subpages/edit_profile.php';
