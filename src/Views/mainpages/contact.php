f52987a3b76fe2a055a251dbe77f872224715406
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-02-21 01:43:46 +0800

    Add contact page main section structure

--- src/Views/mainpages/contact.php
+++ src/Views/mainpages/contact.php
@@ -8,6 +8,47 @@
 </head>
 <body>
     <?php require __DIR__ . "/header.php";?>
-    this is contact page
+    <main class="container">
+        <div style="display: flex; gap: 2rem; flex-wrap: wrap;background-color: white; color: black; padding: 2rem;">
+            <!-- Social Links and Help Card -->
+            <div>
+                <h4 style="color: black;">We're Here to Help</h4>
+                <p style="color: black;">If you have questions, problems, or suggestions related to Re:Claim, send a message.</p>
+
+                <div class="container">
+                    
+                    <div>
+                        <img src="/assets/envelope.svg" alt="envelope icon" width="20">
+                        <span>spark.hub@wvsu.edu.ph</span>
+                    </div>
+
+                    <div>
+                        <img src="/assets/envelope.svg" alt="envelope icon" width="20" style="color: white;">
+                        <span>usc.cite@wvsu.edu.ph</span>
+                    </div>
+
+                    <div>
+                        <img src="/assets/facebook.svg" alt="facebook icon" width="20" style="color: white;">
+                        <span>WVSU - Spark Hub</span>
+                    </div>
+
+                    <div>
+                        <img src="/assets/facebook.svg" alt="facebook icon" width="20" style="color: white;">
+                        <span>WVSU - CITE</span>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- Message Card -->
+            <div>
+                <h6>Send us a message</h6>
+            </div>
+        </div>
+        
+    </main>
+    
+    
 </body>
 </html>
