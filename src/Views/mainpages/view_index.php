56cc8ba698eedbbc29c713ebd5fe369c1bc7409b
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-02-20 23:59:00 +0800

    Add header and PicoCSS to index.php

--- src/Views/mainpages/view_index.php
+++ src/Views/mainpages/view_index.php
@@ -11,9 +11,10 @@ if(!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WVSU ReClaim</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
 </head>
 <body>
-        
+    <?php require __DIR__ . "/header.php"?>
     <h1>Welcome to WVSU ReClaim</h1>
 </body>
 </html>
