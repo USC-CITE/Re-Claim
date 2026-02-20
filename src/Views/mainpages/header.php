9bf345b30a03ffc3df6a9fe51dca6563d835eced
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-02-21 00:10:29 +0800

    Add if else statement to handle dynamic display of signup if no user and (profile and logout) if there is)

--- src/Views/mainpages/header.php
+++ src/Views/mainpages/header.php
@@ -11,4 +11,12 @@
         <a href="/contact">Contact Us</a>
     </nav>
     <!-- Ternary Operator to check if user is logged in display profile and logout if not register-->
+    <?php 
+        if(!isset($_SESSION['user_id'])){
+            
+            echo "<a>Sign up</a>";
+        }else{
+            echo "<a>Profile</a> <br> <a>Logout</a>";
+        }
+    ?>
 </header>
