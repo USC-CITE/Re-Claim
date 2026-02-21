0ff99793c9c3717b7d9e4126d5cfbf7e2060859e
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-02-22 03:52:07 +0800

    Add sending of message from user to organization email AND Add email notification to the sender

--- src/Controllers/ContactController.php
+++ src/Controllers/ContactController.php
@@ -15,5 +15,46 @@ class ContactController{
     public static function showContactPage(){
         require __DIR__ . "/../Views/mainpages/contact.php";
     }
+
+    public static function sendMessage(): array{
+        // Get values
+        $name = $_POST['name'] ?? '';
+        $wvsu_email = trim($_POST['wvsu-email'] ?? '');
+        $message = $_POST['message'] ?? '';
+
+        // Check fields
+        if(!$name || !$wvsu_email || !$message){
+            return ['error' => 'All fields are required'];
+        }
+        // Validate email format
+        if (!str_ends_with($wvsu_email, '@wvsu.edu.ph') || !filter_var($wvsu_email, FILTER_VALIDATE_EMAIL)) {
+                return ['error' => 'Please enter a valid WVSU email'];
+        }
+
+        $adminEmail = 'kellyydrhan.alojepan@wvsu.edu.ph';
+        try{
+            // PHPMailer sending 
+            Mailer::sendMessage(
+                $adminEmail,
+                'ReClaim Admin',
+                "New Message from $name",
+                "Name: $name<br>Email: $wvsu_email<br>Message:$message",
+                $wvsu_email,
+                $name
+            );
+            
+            // Send notification to the sender
+            Mailer::sendMessage(
+                $wvsu_email,               // Student email
+                $name,
+                "ReClaim Contact Form Confirmation",
+                "A message was sent using your email.<br>Message:<br>$message"
+            );
+
+            return ['success' => 'Message sent successfully!'];
+        }catch(Exception $e){
+            return ['error' => 'Failed to send message: ' . $e->getMessage()];
+        }
+    }
 }
 ?>
