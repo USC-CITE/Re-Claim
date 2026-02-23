f0ad36a4cdf898d639d770e6576e3f7e4b10b069
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-02-23 10:01:40 +0800

    Add initial landing page structure

--- src/Views/mainpages/view_index.php
+++ src/Views/mainpages/view_index.php
@@ -8,6 +8,55 @@
 </head>
 <body>
     <?php require __DIR__ . "/header.php"?>
-    <h1>Welcome to WVSU ReClaim</h1>
+    <main class="container">
+        <!-- Hero Section-->
+        <section>
+            <div style="width: 40%">
+                <h1>Seamlessly Track and Recover Your Belongings</h1>
+                <p>
+                    Re:Claim is a centralized lost and found tracking system for West Visayas State University - Main Campus.
+                </p>
+                <a href="/lost">
+                    <button>Find your Lost Item</button>
+                </a>
+            </div>
+        </section>
+
+        <!-- ReClaim Features Section-->
+        <section>
+            <h2>Why Use Reclaim?</h2>
+
+            <!-- Feature Cards -->
+            <div class="container" style="display: flex; gap: 1rem;">
+                <div>
+                    <img src="/public//uploads//found_items//found_20260213_011159_F3CB3B.jpg" style="width: 80%">
+                    <h6>Let people help you: post the item that you lost</h6>
+                    <p>Ensure fast recovery by distributing information online.</p>
+                </div>
+                <div>
+                    <img src="/public//uploads//found_items//found_20260213_011159_F3CB3B.jpg" style="width: 80%">
+                    <h6>Let people help you: post the item that you lost</h6>
+                    <p>Ensure fast recovery by distributing information online.</p>
+                </div>
+                <div>
+                    <img src="/public//uploads//found_items//found_20260213_011159_F3CB3B.jpg" style="width: 80%">
+                    <h6>Let people help you: post the item that you lost</h6>
+                    <p>Ensure fast recovery by distributing information online.</p>
+                </div>
+            </div>
+        </section>
+        <!-- Issue Section -->
+        <section class="container">
+            <!-- Github Screenshot-->
+                <img src="/public/uploads/found_items/found_20260213_011159_F3CB3B.jpg">
+            <div>
+                <h2>Want to Contribute in Improving the System?</h2>
+                <p>Re:Claim is open source! Place an issue on GitHub and we’ll review it.</p>
+                <a href="/">
+                    <button>Place an issue</button>
+                </a>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
