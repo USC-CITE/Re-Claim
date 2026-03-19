9aba66b0b7ea7fc7e9e20609c0270364a6801b77
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-03-20 00:49:52 +0800

    Added upload avatar route to routing system

--- public/index.php
+++ public/index.php
@@ -62,6 +62,7 @@ $router->post('/lost/delay-archive', [LostItemController::class, 'delayArchive']
 $router->get('/profile', [ProfileController::class, 'showProfile']);
 $router->get('/profile/edit', [ProfileController::class, 'showEditProfile']);
 $router->post('/profile/archived/delete', [ProfileController::class, 'deleteArchivedItems']);
+$router->post('/profile/avatar/upload', [ProfileController::class, 'uploadAvatar']);
 
 /* Found Item */
 $router->get('/found', [FoundItemController::class, 'index']);      // List Page
