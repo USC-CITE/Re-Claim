c2a431d77d6ae9d52fcbc65d40a35fde7e30dcb3
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-03-19 19:49:45 +0800

    Add temporary js script for edit profile tabs functionality

---
+++ public/js/profile/editProfileTabs.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".tab-btn");
+    const tabs = document.querySelectorAll(".tab-content");
+
+    function activateTab(tabId) {
+
+        // hide all tabs
+        tabs.forEach(tab => {
+            tab.classList.remove("active");
+        });
+
+        // deactivate all buttons
+        buttons.forEach(btn => {
+            btn.classList.remove("active");
+        });
+
+        // activate selected tab
+        document.getElementById(tabId).classList.add("active");
+
+        // activate selected button
+        document.querySelector(`[data-tab="${tabId}"]`).classList.add("active");
+    }
+
+    // default tab
+    activateTab("edit-profile");
+
+    // click events
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            activateTab(this.dataset.tab);
+        });
+    });
+});
