05ab46c2a88cec7106e7f675d44af74a52ba8c86
Mae Daniella Faith Benjamin <maedaniellafaith.benjamin@wvsu.edu.ph>
2026-03-21 21:50:02 +0800

    Change listing pages to load app.css

--- src/Views/lost/index.php
+++ src/Views/lost/index.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Lost Items</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+  <link rel="stylesheet" href="/css/app.css">
 </head>
-<body>
+<body class="font-poppins bg-white text-primary min-h-screen">
 
 <main class="container">
   <?php require __DIR__ . "/../mainpages/header.php"?>
