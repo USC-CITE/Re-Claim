0e89ca2b7b9695e8c36575606c4756c15d020376
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2026-04-21 21:39:20 +0800

    Cut tailwind import script

--- src/Views/auth/register.php
+++ src/Views/auth/register.php
@@ -14,7 +14,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="/css/app.css">
     <script src="/js/auth/password_toggle.js" defer type="module"></script>
     <title>WVSU ReClaim - Create Account</title>
