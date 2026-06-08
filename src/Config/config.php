25cb51870aabb8356587082eff930b039433c77f
Keane Emanuel Dalisay <keandals21@gmail.com>
2026-06-08 11:29:07 +0800

    Fix import of environment variables via config

--- src/Config/config.php
+++ src/Config/config.php
@@ -5,7 +5,9 @@
  * Rules: Hide sensitive information
  */
 
-$envPath = dirname(__DIR__, 2) .'/.env';
+use Dotenv\Dotenv;
+
+$envPath = dirname(__DIR__, 2);
 
 // Handle error if file does not exist
 if (!file_exists($envPath)) {
@@ -14,15 +16,16 @@ if (!file_exists($envPath)) {
 }
 
 // Parse the fields inside the .env into an array
-$env = parse_ini_file($envPath);
+// $env = parse_ini_file($envPath);
+Dotenv::createImmutable($envPath, '.env')->load();
 
 return[
     'db' => [
-        'host' => $env['DB_CONNECTION'] ?? 'localhost',
-        'port' => $env['DB_PORT'] ?? 3306,
-        'name' => $env['DB_NAME'] ?? null, // must comde from .env
-        'user' => $env['DB_USER'] ?? null, // must comde from .env
-        'pass' => $env['DB_PASS'] ?? null, // must comde from .env
-        'charset' => $env['DB_CHARSET'] ?? 'utf8mb4 ',
+        'host' => $_ENV['DB_HOST'] ?? 'localhost',
+        'port' => $_ENV['DB_PORT'] ?? 3306,
+        'name' => $_ENV['DB_NAME'] ?? null, // must comde from .env
+        'user' => $_ENV['DB_USER'] ?? null, // must comde from .env
+        'pass' => $_ENV['DB_PASS'] ?? null, // must comde from .env
+        'charset' => $_ENV['DB_CHARSET'] ?? 'utf8mb4 ',
     ],
 ];
